046b870ae7e3ea10699fed32bcd4abd8d2f62219
George Sanders <32531451+gsanders1800@users.noreply.github.com>
2019-08-01 18:23:53 -0400

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -23,8 +23,6 @@ import java.util.regex.*;
 public class MainActivity extends AppCompatActivity {
 
 
-String gameState;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,11 +33,57 @@ String gameState;
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //TextViews
+                //Items in main activity
                 TextView Hourly = (TextView) findViewById(R.id.txtNetHourly);
                 TextView Daily = (TextView) findViewById(R.id.txtNetDaily);
                 TextView BiWeekly = (TextView) findViewById(R.id.txtNetBiweekly);
-                TextView Monthly= (TextView) findViewById(R.id.txtNetMonthly);
+                TextView Monthly = (TextView) findViewById(R.id.txtNetMonthly);
+                EditText State = (EditText) findViewById(R.id.txtState);
+                EditText GrossSal = (EditText) findViewById(R.id.txtSalary);
+                RadioButton Single = (RadioButton) findViewById(R.id.RbtnSingle);
+                RadioButton HeadOfHouse = (RadioButton) findViewById(R.id.RbtnHeadOfHousehold);
+                RadioButton Married = (RadioButton) findViewById(R.id.RbtnMarried);
+                TextView Warn = (TextView) findViewById(R.id.lblWarning);
+
+                //Variables
+                double GrossSalary;
+                double NetSalary, NetMonthly, NetBiWeekly, NetDaily, NetHourly;
+                double FedTaxRate, StateTaxRate, SSRate, MedicareRate;
+                String StateName, MaritalStat, Salary;
+
+                //Get Salary from individual and convert to double
+                Salary = GrossSal.toString();
+                GrossSalary =  Double.parseDouble(Salary);
+
+                //Get name of the stat & deny invalid entry (MUST CONTINUE)
+                StateName = State.toString();
+
+
+
+                //Get Marital Status
+                if(Single.isChecked()) {
+                    MaritalStat = "Single";
+                }
+                else if(HeadOfHouse.isChecked()) {
+                    MaritalStat = "Head Of Household";
+                }
+                else if(Married.isChecked()){
+                    MaritalStat = "Married";
+                }
+                else{
+                    Warn.setVisibility(View.VISIBLE);
+                    Warn.setText("Warning: Please select a marital Status");
+                }
+
+                //Determine what bracket the individual is in with salary range
+                if(GrossSalary > 0 && GrossSalary <= 9525){
+
+                }
+
+                //New Jersey State Tax
+                if(StateName == "NJ" || StateName == "nj"){
+
+                }
 
 
             }
